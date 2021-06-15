d4aee672f02134cb7d1c9aa6ea57c79add7fbbcd
Clément <45621549+Clement-E@users.noreply.github.com>
2021-06-15 17:09:24 +0200

    Initial commit

---
+++ postCss.config.js
@@ -0,0 +1,15 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  mode: 'jit',
+  theme: {
+    extend: {
+      colors: {
+        'light-blue': colors.lightBlue,
+        cyan: colors.cyan,
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
